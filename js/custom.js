d522ecefdea37c98869292b0d7a7fbc55906448d
= <=abdoelsimy123@gmail.com>
2019-09-16 03:33:01 +0200

    final touches

--- js/custom.js
+++ js/custom.js
@@ -27,14 +27,12 @@ for (i = 0; i < tabcontent.length; i++) {
     }
 }
 function openForm(evt, typeForm) {
-    console.log(typeForm);
     var i, tabcontent, tablinks;
     tabcontent = document.getElementsByClassName("tabcontent");
     for (i = 0; i < tabcontent.length; i++) {
         tabcontent[i].style.display = "none";
     }
     tablinks = document.getElementsByClassName("tablinks");
-    console.log(tablinks.length);
     for (i = 0; i < tablinks.length; i++) {
         tablinks[i].className = tablinks[i].className.replace(" active", "");
     }
