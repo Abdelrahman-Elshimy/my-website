6fa8c74791c7bc3fa184344fe52ba714d4945290
= <=abdoelsimy123@gmail.com>
2019-09-16 03:31:04 +0200

    contact and footer section and complete ui

--- js/custom.js
+++ js/custom.js
@@ -3,10 +3,42 @@
 // play in my name
 
 var counter = 0;
-setInterval(function() {
-    $('.top .profile-name h1 span:eq('+ counter +')').css({
+setInterval(function () {
+    $('.top .profile-name h1 span:eq(' + counter + ')').css({
         "animation": "beBig .5s ease-in-out",
         "display": "inline-block"
     });
     counter++;
 }, 500);
+
+
+// $(document).ready(function(){
+//     $(".tabs-types p").click(function(){
+//       $(this).addClass('active').siblings().removeClass('active');
+//     });
+//   });
+tabcontent = document.getElementsByClassName("tabcontent");
+for (i = 0; i < tabcontent.length; i++) {
+    if (i == 0) {
+        tabcontent[i].style.display = "block";
+    }
+    else {
+        tabcontent[i].style.display = "none";
+    }
+}
+function openForm(evt, typeForm) {
+    console.log(typeForm);
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    console.log(tablinks.length);
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(typeForm).style.display = "block";
+    evt.currentTarget.className += " active";
+}
+
